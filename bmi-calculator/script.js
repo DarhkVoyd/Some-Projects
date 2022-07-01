d54ac6b24ef8690bccfa5cf779adc9c0284b0841
D V <divyadityasnaruka@gmail.com>
2022-07-01 19:48:53 +0530

    result text added

--- bmi-calculator/script.js
+++ bmi-calculator/script.js
@@ -16,10 +16,28 @@ function calculate() {
     heightValue = parseFloat(height.value);
     weightValue = parseFloat(weight.value);
 
-    if (!heightValue || !weightValue)
+    if (!heightValue || !weightValue || heightValue < 0 || weightValue < 0)
     {
         alert('Enter valid value!!');
         return 1;
     }
-    document.getElementById('result').textContent = `Result: ${(weightValue / (heightValue * heightValue)).toFixed(2)}`;
+
+    let bmi = (weightValue / (heightValue * heightValue)).toFixed(2);
+    let result = '';
+    if (bmi < 18.5)
+    {
+        result = 'Underweight';
+    }
+    else if (bmi >= 18.5 && bmi < 25)
+    {
+        result = 'Normal';
+    }
+    else if (bmi >= 25 && bmi < 30)
+    {
+        result = 'Overweight';
+    }
+    else {
+        result = 'Obese'
+    }
+    document.getElementById('result').textContent = `Result: ${bmi} -> ${result}`;
 } 
